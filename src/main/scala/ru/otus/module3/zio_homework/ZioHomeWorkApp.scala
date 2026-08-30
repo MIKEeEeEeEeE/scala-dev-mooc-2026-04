7186264ee5feb5f3adef9a98b9ea4f51681fdf8b
mike <kuzmish02@gmail.com>
2026-08-30 11:59:33 +0200

    hw-8-zio-effects

--- src/main/scala/ru/otus/module3/zio_homework/ZioHomeWorkApp.scala
+++ src/main/scala/ru/otus/module3/zio_homework/ZioHomeWorkApp.scala
@@ -4,6 +4,6 @@ import zio.{Scope, Unsafe, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 object ZioHomeWorkApp extends ZIOAppDefault{
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = Unsafe.unsafe { implicit unsafe =>
-    runtime.unsafe.run(guessProgram)
+    runtime.unsafe.run(doWhile(guessProgram))
   }
 }
